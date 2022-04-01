69ab5112efb1176d7da1fa36a1e4ad3572e6f2c4
VoCongKhoa <vocongkhoa2208@gmail.com>
2022-04-01 19:16:45 +0700

    up bt

---
+++ ss5_thao_tac_voi_csdl_va_orm/thuc_hanh/customer_management_orm/src/main/java/project/initial/AppConfig.java
@@ -0,0 +1,66 @@
+package project.initial;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.lang.management.PlatformLoggingMXBean;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan("project")
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/customers_demo?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        return properties;
+    }
+
+    @Bean
+    @Qualifier(value = "entityManager")
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan("project");
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setJpaProperties(additionalProperties());
+        return entityManagerFactoryBean;
+    }
+
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
---
+++ ss5_thao_tac_voi_csdl_va_orm/thuc_hanh/test/src/main/java/com/codegym/model/repository/impl/StudentRepository.java
@@ -0,0 +1,45 @@
+package com.codegym.model.repository.impl;
+
+import com.codegym.model.bean.Student;
+import com.codegym.model.repository.IStudentRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class StudentRepository implements IStudentRepository {
+
+    @Override
+    public void save(Student student) {
+        EntityTransaction transaction = BaseRepository.entityManager.getTransaction();
+        transaction.begin();
+        BaseRepository.entityManager.persist(student);
+        transaction.commit();
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> typedQuery = BaseRepository.entityManager.createQuery(
+                "select s " +
+                        "from student as s" , Student.class);
+//        typedQuery.setParameter(1, id);
+
+        return typedQuery.getResultList();
+//        return BaseRepository.entityManager.createQuery(
+//                "SELECT s FROM student as s", Student.class).getResultList();
+    }
+
+    @Override
+    public Student findById(Integer id) {
+//         TypedQuery<Student> typedQuery =  BaseRepository.entityManager.createQuery(
+//                "select s " +
+//                        "from student s " +
+//                        "where s.id = ?1", Student.class);
+//         typedQuery.setParameter(1, id);
+
+//         return typedQuery.getSingleResult();
+        return BaseRepository.entityManager.find(Student.class, id);
+    }
+}
